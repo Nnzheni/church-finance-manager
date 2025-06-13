0d6f53f34fc3c34097e6985f5b306ad01943387c
Nnzheni <mukundigis@gmail.com>
2025-06-13 21:08:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed.');
-  self.skipWaiting();
-});
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
-});
-
-self.addEventListener('fetch', event => {
-  // You can add offline caching here if needed
-});
